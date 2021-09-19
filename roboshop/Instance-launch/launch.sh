a1c6a573ef55c22470753bace519e2bd644302d5
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 23:23:56 +0800

    just commit

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -10,14 +10,26 @@ exit 1
 fi
 
   INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
-  if [ "${INSTANCE_STATE}" = "running" ]; then
-    echo "${COMPONENT} Instance already exists!!"
-  exit 0
-  fi
 
-  if [ "${INSTANCE_STATE}" = "stopped" ]; then
-    echo "${COMPONENT} Instance already exists!!"
-    exit 0
-  fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LTId},Version=${LTVER} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]"
+
+INSTANCE_STATE | jq -c '.[]' | while read STATE_CODE; do
+    api_call "$STATE_CODE"
+done
+
+t_count=0
+exists_count=0
+   for  STATE in "${STATE_CODE[@]}"; do
+     if [ "$STATE" == 48  ]; then
+   echo "Instance Terminated "
+      t_count=t_count+1
+      echo "$STATE"
+   else
+     echo "Instance ${COMPONENT} not exists "
+     exists_count=exists_count+1
+     echo "$STATE"
+   fi
+done
+
+echo " t_count $t_count"
+echo " exists_count $exists_count"
