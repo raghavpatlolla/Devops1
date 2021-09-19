7d4fcbd6197d08a311610ad45978beef0119f552
patlollaraghav <patlollaraghav@gmail.com>
2021-09-20 00:21:27 +0800

    just commit

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -10,25 +10,21 @@ exit 1
 fi
 
   INSTANCE_STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Code | xargs )
-
-
- t_count=0
- exists_count=0
-echo  "$INSTANCE_STATE"
-borkstring=$(echo  "$INSTANCE_STATE"| sed -e 's/\[ //g' -e 's/\ ]//g' -e 's/\,//g')
-arr=( $borkstring )
+t_count=0
+exists_count=0
+STATE_STRING=$(echo  "$INSTANCE_STATE"| sed -e 's/\[ //g' -e 's/\ ]//g' -e 's/\,//g')
+arr=( $STATE_STRING)
 for STATE in ${arr[@]}; do
-    echo ${STATE//\"}
-    if [ "$STATE" == 48  ]; then
-   echo "Instance Terminated "
+     if [ "$STATE" == 48  ]; then
       t_count=`expr $t_count + 1`
-      echo "$STATE"
-   else
-     echo "Instance ${COMPONENT} not exists "
+    else
      exists_count=`expr $exists_count + 1`
-     echo "$STATE"
-   fi
+      fi
 done
 
-echo " t_count $t_count"
-echo " exists_count $exists_count"
+if [ "$exists_count" -gt 0 ]; then
+echo "Instance ${COMPONENT} already exists "
+exit 0
+else
+  echo "Instance ${COMPONENT} not exists "
+fi
