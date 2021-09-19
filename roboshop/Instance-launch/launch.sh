cda84b331a3de7f136979b6bb27a9c36c88f7e69
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 22:52:23 +0800

    just commit

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -9,15 +9,23 @@ exit 1
 
 fi
 
-
+allThreads=(1 2 4 8 16 32 64 128)
 
   INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
   STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=frontend  | jq .Reservations[].Instances[].State.Code)
 
 echo ${STATE}
 
-   if [ "$STATE" == "48"  ]; then
+t_count=0
+exists_count=0
+   for  STATE in "${INSTANCE_EXISTS[@]}"; do
+     if [ "$STATE" == "48"  ]; then
    echo "Instance Terminated "
+      t_count=t_count+1
+      echo "$STATE"
    else
      echo "Instance ${COMPONENT} not exists "
+     exists_count=exists_count+1
+     echo "$STATE"
    fi
+done
