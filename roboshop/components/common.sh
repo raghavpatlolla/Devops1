fb392adabbbd8c4ba5af82a06feb7afeef40ff28
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 14:32:35 +0800

    just commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -5,6 +5,7 @@ HEAD(){
 STAT(){
   if [ $1 -eq 0 ]; then
     echo -e "\e[1;32m SUCCESS \e[0m"
+      echo -e "\e[1;32m SUCCESS from two \e \033[36m%-30s\033[0m %s\n"
   else
     echo -e "\e[1;31m FAILED \e[0m"
     echo -e "\e[1;33m Check the log for more details...Log-file: /tmp/roboshop.log\e[0m"
@@ -13,4 +14,4 @@ STAT(){
 
 }
 
-##"\033[36m%-30s\033[0m %s\n",
+
