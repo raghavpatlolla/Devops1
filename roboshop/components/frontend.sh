553c7b6c0bb615454d98f56b9ccedf710d010a1f
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 14:35:13 +0800

    just commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,3 +9,7 @@ STAT $?
 HEAD  "Enabling and starting NGINX"
 systemctl enable nginx && systemctl start nginx &>>$log
 STAT $?
+
+HEAD  "Download frontend from github"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+STAT $?
