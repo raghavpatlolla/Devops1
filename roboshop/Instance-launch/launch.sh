aa3ff30a10987bb26dfd0264a5d141aea3d85fdd
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 18:39:15 +0800

    just commit

---
+++ roboshop/Instance-launch/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+COMPONENT=$1
+LTVER=2
+LTId=lt-0d5e63f293cae8014
+
+if [ -z "${COMPONENT}" ]; then
+echo "COMPONENT input is Needed"
+exit 1
+
+fi
+aws ec2 run-instances --launch-template LaunchTemplateId=${LTId},Version=${LTVER} --tag-specifications 'ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]'
