6f5f053d7e0bbae1aa678fdb9ebdf4ba8feacdc9
patlollaraghav <patlollaraghav@gmail.com>
2021-09-20 00:29:00 +0800

    just commit

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -9,11 +9,21 @@ exit 1
 
 fi
 
-  INSTANCE_STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Code | xargs )
+INSTANCE_STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Code | xargs )
+###INSTANCE_STATE is JSON array
+###Converting a JSON array to a bash array
 t_count=0
 exists_count=0
 STATE_STRING=$(echo  "$INSTANCE_STATE"| sed -e 's/\[ //g' -e 's/\ ]//g' -e 's/\,//g')
 arr=( $STATE_STRING)
+## arr is bash array
+## looping bash arry and counting the instance code
+##0 : pending
+##16 : running
+##32 : shutting-down
+##48 : terminated
+##64 : stopping
+##80 : stopped
 for STATE in ${arr[@]}; do
      if [ "$STATE" == 48  ]; then
       t_count=`expr $t_count + 1`
@@ -27,4 +37,5 @@ echo "Instance ${COMPONENT} already exists "
 exit 0
 else
   echo "Instance ${COMPONENT} not exists "
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LTId},Version=${LTVER} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]"
 fi
