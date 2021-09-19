df36823fea64fecd78431b7fafdb9d14db4aea84
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 14:26:56 +0800

    just commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,5 +3,9 @@ source components/common.sh
 log=/tmp/roboshop.log
 rm -rf $log
 HEAD  "Installing NGINX"
+amazon-linux-extras install nginx1&>>$log
 yum install nginx -y &>>$log
+STAT $?
+HEAD  "Enabling and starting NGINX"
+systemctl enable nginx && systemctl start nginx
 STAT $?
