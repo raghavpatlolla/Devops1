029427ad45b46a794a95a99c90e805e6b7edd700
patlollaraghav <patlollaraghav@gmail.com>
2021-09-19 14:04:29 +0800

    just commit

---
+++ roboshop/components/common.sh
@@ -0,0 +1,16 @@
+HEAD(){
+  echo -n -e "\e[1m $1 \e[0m \t\t ... "
+}
+
+STAT(){
+  if [$1 -eq 0]; then
+    echo -e "\e[1;32m SUCCESS \e[0m"
+  else
+    echo -e "\e[1;31m FAILED \e[0m"
+    echo -e "\e[1;33m Check the log for more details...Log-file: /tmp/roboshop.log\e[0m"
+    exit 1
+  fi
+
+}
+
+##"\033[36m%-30s\033[0m %s\n",
