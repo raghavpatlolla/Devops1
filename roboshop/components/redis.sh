a0978d78d6504e42bf4e39b1ec8a26e652485428
patlollaraghav <patlollaraghav@gmail.com>
2021-09-21 01:54:53 +0800

    just commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Installing Redis"
+source components/common.sh
+log=/tmp/roboshop.log
+rm -rf $log
+HEAD  "Setting up Redis pre-requisites "
+amazon-linux-extras install epel -y &>>$log
+STAT $?
+HEAD  "Installing Redis "
+yum install epel-release yum-utils -y &>>$log &&
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$log&&
+yum-config-manager --enable remi &>>$log&&
+yum install redis -y &>>$log
+STAT $?
+HEAD  "Update BindIP"
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf  &>>$log &&
+sed -i -e "s/127.0.0.1/0.0.0.0/"  /etc/redis/redis.conf  &>>$log
+STAT $?
+HEAD  "Start Redis Database"
+systemctl enable redis && systemctl start redis &>>log
+STAT $?
