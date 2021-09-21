ec2a256615e5340060e67212c8ffcac403f664ad
patlollaraghav <patlollaraghav@gmail.com>
2021-09-21 18:38:29 +0800

    just commit

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -7,6 +7,7 @@ yum install maven -y &>>$log
 STAT $?
 ADD_USER
 HEAD  "Downloading from Github "
+ cd /home/roboshop &&
  curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip" &&
  unzip /tmp/shipping.zip  &>>$log &&
  mv shipping-main shipping &&
