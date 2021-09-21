a9adf20377614a8eb51f106e2bc01758067981fd
patlollaraghav <patlollaraghav@gmail.com>
2021-09-21 18:53:05 +0800

    just commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "Installing RabbitMQ"
+source components/common.sh
+log=/tmp/roboshop.log
+rm -rf $log
+HEAD  "Setting up rabbitmq pre-requisites "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>$log
+STAT $?
+
+HEAD  "YUM repositories for RabbitMQ"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>$log
+STAT $?
+HEAD  "Install RabbitMQ"
+yum install rabbitmq-server -y  &>>$log
+STAT $?
+
+SET_SYSTEMD_SERVICE "rabbitmq-server"
+
+HEAD  "Create RabbitMQ application user"
+rabbitmqctl add_user roboshop roboshop123 &>>$log && rabbitmqctl set_user_tags roboshop administrator &>>$log  && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log
+STAT $?
+
