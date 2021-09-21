710eeebe7aeb87d8c4fb1c5e76f29dc0cc46d5ae
patlollaraghav <patlollaraghav@gmail.com>
2021-09-21 16:10:07 +0800

    just commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -53,8 +53,7 @@ chown -R  roboshop:roboshop /home/roboshop/ &>>$log
 STAT $?
 
 HEAD  "Setup systemd.service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'   -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/'
- /home/roboshop/$1/systemd.service &>>$log && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service &>>$log
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'   -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/'  /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
 STAT $?
 HEAD  "Setup systemd.service"
  systemctl daemon-reload &&
